e2cc789dcc3bd73d83145a6ced05016e289c0565
Quazia <alf40k@gmail.com>
2014-03-25 14:37:39 -0400

    Update Source.cpp
    This version will handle the following input::
    F
    - 5.2 3.1      
    + 7 2.5       
    * 3 5 
    / 5 0             
    / 6 2	    
    % 8.3 5.1 
    ^ 2 3	    
    E
    S 40  
    f
    S 40
    C 60      	
    e
    T 30
    
    Still need ways of handling '•' and "(times)"
    There also seems to be an anomaly in the line break at the line "%  8.3       5.1           " that prevents getline() from advancing.

--- Lab6/Source.cpp
+++ Lab6/Source.cpp
@@ -16,7 +16,7 @@ using namespace std;
 static const double PI = 3.14159;
 static const int MAX_LENGTH = 20;
 //-------------------------------------------------------------------------------------------------
-void readOperation(const char readLine[MAX_LENGTH], int& index, ifstream& input, 
+void readOperation(const char readLine[MAX_LENGTH], ifstream& input,
 	ofstream& output);
 void arithmetic(const char readLine[MAX_LENGTH], char operation, int& index, ifstream& input, ofstream& output);
 int getTotalWords(const char currentLine[MAX_LENGTH]);
@@ -28,68 +28,68 @@ void cleanToNext(const char readLine[MAX_LENGTH], char currentLine[MAX_LENGTH], 
 int main(){
 	char readLine[MAX_LENGTH];
 	int index = 0;
-	
+
 	ifstream input;
 	input.open("PFN.txt");
 	if (input.fail())	//check to see if the input file is there, if it isnt then
 		exit(1);		//close this program instantly
 	ofstream output;
 	output.open("Output.txt");
-	
-	while (input.getline(readLine, MAX_LENGTH)){
-		readOperation(readLine, index, input, output);
+
+	while (!(input.eof())){
+		input.getline(readLine, MAX_LENGTH);
+		readOperation(readLine, input, output);
+		std::cout << "Next Line";
 	}//end while loop
-	
+
 	input.close();
 	output.close();
 	system("pause");
 	return 0;
 }//end main
 
-void readOperation(const char readLine[MAX_LENGTH], int& index, ifstream& input, ofstream& output){
-	char next = readLine[0];
-	
-	while (next != '\n'){
+void readOperation(const char readLine[MAX_LENGTH], ifstream& input, ofstream& output){
+	int doContinue=0;
+	int index = 0;
+	char next = readLine[index];
+
+	while (doContinue!=1){
+		std::cout << index;
 		if (isalpha(next)){
+			std::cout << "do trig";
 			if (next == 'S' || next == 'T' || next == 'C'){
 				trigFunction(readLine, next, index, input, output);
-				index = 1;
+				doContinue = 1;
 			}
-			else
+			else{
 				modeToPrint(readLine, next, output);
+				doContinue = 1;
+			}
 		}
-		else if (isprint(next)){
+		else if (isprint(next) || (next == '•')){
+			std::cout << "do arithmatic";
 			arithmetic(readLine, next, index, input, output);
-			index = 1;
+			doContinue = 1;
 		}
+		index++;
+		next = readLine[index];
 	}
 }
 
 void readNumber(const char readLine[MAX_LENGTH], double numArray[2], int& index, ifstream& input){
 	double answer = 0;
-	int i = 0, j = 0;
-	char digit[MAX_LENGTH];
-	char next;
-	input.get(next);
-	input.get(next);
-
-	for (int k = 1; k < (strlen(readLine) - 1); k++){
-		if (isdigit(next) || ispunct(next)){
-			digit[i] = next;
-			cout << digit[i] << " " << endl;
-			i++;
-			input.get(next);
+	int j = 0, i = 0;
+	string digits = "";
+	for (unsigned int k = 0; (j!=2) && (k < (strlen(readLine) - 1)); k++){
+		if (isdigit(readLine[k]) || (readLine[k]=='.')){
+			digits+=readLine[k];
+			if (!(isdigit(readLine[k + 1]) || (readLine[k + 1] == '.'))){
+				std::cout << digits << endl;
+				answer = atof(digits.c_str());
+				numArray[j++] = answer;
+				digits = "";
+			}
 		}//end if
-		else if (isspace(next)){
-			digit[i] = '\0';
-			answer = atof(digit);
-			numArray[j] = answer;
-			j++;
-			while (isspace(next)){
-				input.get(next);
-				i = 0;
-			}//end while
-		}//end else if
 	}//end for
 }//end readNumber Function
 
@@ -98,36 +98,39 @@ void arithmetic(const char readLine[MAX_LENGTH], char operation, int& index, ifs
 	readNumber(readLine, numArray, index, input);
 	double numberOne = numArray[0];
 	double numberTwo = numArray[1];
-	cout << numArray[0] << " " << numArray[1] << endl;
+	std::cout << numArray[0] << " " << numArray[1] << endl;
 	double answer;
 	switch (operation) {
-		case '+':
-			answer = numberOne + numberTwo;
-			output << "+ " << numberOne << " " << numberTwo;
-			break;
-		case '-':
-			answer = numberOne - numberTwo;
-			output << "- " << numberOne << " " << numberTwo;
-			break;
-		case '*':
-			answer = numberOne * numberTwo;
-			output << "* " << numberOne << " " << numberTwo;
-			break;
-		case '/':
-			answer = numberOne / numberTwo;
-			output << "/ " << numberOne << " " << numberTwo;
-			break;
-		case '%':
-			answer = static_cast<int>(numberOne) % static_cast<int>(numberTwo);
-			break;
-		case '^':
-			answer = pow(numberOne, numberTwo);
-			break;
-		default:
-			answer = 0;
-			break;
+	case '+':
+		answer = numberOne + numberTwo;
+		std::cout << "+ " << numberOne << " " << numberTwo;
+		break;
+	case '-':
+		answer = numberOne - numberTwo;
+		std::cout << "- " << numberOne << " " << numberTwo;
+		break;
+	case '•':
+	case '*':
+		answer = numberOne * numberTwo;
+		std::cout << "* " << numberOne << " " << numberTwo;
+		break;
+	case '/':
+		answer = numberOne / numberTwo;
+		std::cout << "/ " << numberOne << " " << numberTwo;
+		break;
+	case '%':
+		answer = static_cast<int>(numberOne) % static_cast<int>(numberTwo);
+		std::cout << "% " << numberOne << " " << numberTwo;
+		break;
+	case '^':
+		std::cout << "^ " << numberOne << " " << numberTwo;
+		answer = pow(numberOne, numberTwo);
+		break;
+	default:
+		answer = 0;
+		break;
 	}//end switch
-	output << "      " << answer << endl;
+	std::cout << "      " << answer << endl;
 }//end arthmetic Function
 
 void trigFunction(const char readLine[MAX_LENGTH], char operation, int& index, ifstream& input, ofstream& output) {
@@ -136,34 +139,34 @@ void trigFunction(const char readLine[MAX_LENGTH], char operation, int& index, i
 	double number = numArray[0];
 	double answer;
 	switch (operation) {
-		case 'S':
-			answer = sin(PI*number/180);
-			cout << "S" << number;
-			break;
-		case 'C':
-			answer = cos(PI*number/180);
-			cout << "C" << number;
-			break;
-		case 'T':
-			answer = tan(PI*number/180);
-			output << "T" << number;
-			break;
-		default:
-			answer = 0;
-			break;
+	case 'S':
+		answer = sin(PI*number / 180);
+		std::cout << "S" << number;
+		break;
+	case 'C':
+		answer = cos(PI*number / 180);
+		std::cout << "C" << number;
+		break;
+	case 'T':
+		answer = tan(PI*number / 180);
+		std::cout << "T" << number;
+		break;
+	default:
+		answer = 0;
+		break;
 	}//end switch
-	output << "     " << answer << endl;
+	std::cout << "     " << answer << endl;
 }//end trigFunction Function
 
 void modeToPrint(const char readLine[MAX_LENGTH], char next, ofstream& output) {
 	switch (next) {
-		case 'e':
-		case 'E':
-			output << "E" << std::scientific << endl;
-			break;
-		case 'f':
-		case 'F':
-		default:
-			output << "F" << std::fixed << endl;
+	case 'e':
+	case 'E':
+		std::cout << "E" << std::scientific << endl;
+		break;
+	case 'f':
+	case 'F':
+	default:
+		std::cout << "F" << std::fixed << endl;
 	}//end switch
 }//end modeToPrint function
