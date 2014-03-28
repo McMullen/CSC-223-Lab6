321a9bee012988f084e8c69227747f67bfde9322
jjm35205 <jjm35205@live.com>
2014-03-28 17:49:29 -0400

    Added a lot more comments

--- Lab6/Source.cpp
+++ Lab6/Source.cpp
@@ -1,8 +1,8 @@
 //-------------------------------------------------------------------------------------------------
-//Author: Jason McMullen and Arthur Lunn
-//Due Date: 4/1/14
-//Program: Lab6
-//Description:
+//Author:	   Jason McMullen and Arthur Lunn
+//Due Date:	   4/1/14
+//Program:	   Lab6
+//Description: T
 //-------------------------------------------------------------------------------------------------
 #include <fstream>
 #include <iostream>
@@ -13,36 +13,123 @@
 #include <string>
 using namespace std;
 
-static const double PI = 3.14159;
-static const int MAX_LENGTH = 30;
+static const double PI = 3.14159;		//constant to represent our approx. value for PI
+static const int MAX_LENGTH = 30;		//the length of the cstring array
+
+//-------------------------------------------------------------------------------------------------
+//readOperation: This function will read the char at the first index in the cstring array and 
+//				 determine the appropriate mathematical operation to be perfromed on the given
+//				 numerical values, located in the same cstring following the operation marker.
+//				 After determining the operation, the function will pass control of the program
+//				 to the corresponding function to perform the operation.
+//
+//Precondition:  This function needs the cstring array and the object of ifstream type sent to it.
+//Postcondition: This program will not return anything.
 //-------------------------------------------------------------------------------------------------
 void readOperation(const char readLine[MAX_LENGTH], ifstream& input);
+
+//-------------------------------------------------------------------------------------------------
+//arithmetic:    This function will perform the basic arithmetic operations on the two values sent
+//				 to it and then ouput the result. The function will use a switch statement to
+//				 determine what operation will be performed based on the char in the first index
+//				 of the cstring array.
+//
+//Precondition:  The function will need the cstring, the char at the first index of the cstring
+//				 array, and the index of the cstring.
+//Postcondition: The function will not return anything, but it will output the results of the 
+//				 operation.
+//-------------------------------------------------------------------------------------------------
 void arithmetic(const char readLine[MAX_LENGTH], char operation, int& index);
-int getTotalWords(const char currentLine[MAX_LENGTH]);
+
+//-------------------------------------------------------------------------------------------------
+//readNumber:	 This function will find all of the numbers remaining in the cstring array, store
+//				 them in a new cstring array and then convert those cstrings into doubles. It will
+//				 then store those doubles into an array of doubles that the calling function has
+//				 access to. This function uses the cleanToNext function to take care of the white
+//				 spaces separating the numbers.
+//
+//Precondition:	 This function will require the cstring array, the array of type double, and the 
+//				 current index of the cstring array.
+//Postcondition: This function will not return anything.
+//-------------------------------------------------------------------------------------------------
 void readNumber(const char readLine[MAX_LENGTH], double numArray[2], int& index);
+
+//-------------------------------------------------------------------------------------------------
+//trigFunction:  This function will view the char holding the value that stands for the operation
+//				 needed to be performed. Using a switch statement, the function will match this
+//				 char with 'S', 'C', and 'T' and after finding the matching char, that trig
+//				 function will be selected. The function will then call readNumber to determine
+//				 the degree value that is within the cstring array. After recieving the value,
+//				 double is then converted from degree to radians using the following formula:
+//										PI(number)/180.
+//				 The function will then do the trig operation on the radian and finally output the
+//				 solution. 
+//
+//Precondition:  The function requires the cstring, first char in the cstring and the current
+//				 index of the cstring array to be sent to it.
+//Postcondition: The function will not return anything, but will change the value of the index
+//			 for the calling function.
+//-------------------------------------------------------------------------------------------------
 void trigFunction(const char readLine[MAX_LENGTH], char operation, int& index);
+
+//-------------------------------------------------------------------------------------------------
+//modeToPrint:   This function will change the way decimal values are represented. A switch 
+//				 statement will check the char sent from readOperation and determine which decimal
+//			     structure should be used to print doubles. If an 'e' or 'E' is found, decimals
+//				 will be displayed in scientific notation. If a 'f' or 'F' is found, decimals will
+//				 be displayed with a fixed number of digits.
+//
+//Precondition:  The function requires a cstring and the char read from the first index of the 
+//				 cstring.
+//Postcondition: The function will not return anything. It will however, change the way all other
+//				 functions will print double variables.
+//-------------------------------------------------------------------------------------------------
 void modeToPrint(const char readLine[MAX_LENGTH], char next);
+
+//-------------------------------------------------------------------------------------------------
+//cleanToNext:
+//
+//-------------------------------------------------------------------------------------------------
 void cleanToNext(const char readLine[MAX_LENGTH], char currentLine[MAX_LENGTH]);
 
+//-------------------------------------------------------------------------------------------------
+//main:			 The main function will drive the program and make all of the necessary function
+//				 calls. It also has the responsibility of making a connection with the input txt
+//				 file and reading in the entire document line by line.
+//
+//Precondition:  main does not require anything to be sent to it for it to run.
+//Postcondition: This function will return the value 0 letting the system know the program has
+//				 finished.
+//-------------------------------------------------------------------------------------------------
+
 int main(){
-	char readLine[MAX_LENGTH];
-	int index = 0;
+	char readLine[MAX_LENGTH];//cstring array to hold, line by line, the contents of the txt file
 
 	ifstream input;
 	input.open("PFN.txt");
-	if (input.fail())	//check to see if the input file is there, if it isnt then
-		exit(1);	//close this program instantly
+	if (input.fail())		  //check to see if the input file is there, if it isnt then
+		exit(1);			  //close this program instantly
 
-	while (!(input.eof())){
+	while (!(input.eof())){	  //continue reading the file until empty
 		input.getline(readLine, MAX_LENGTH);
 		readOperation(readLine, input);
 	}//end while loop
 
-	input.close();
-	system("pause");
+	input.close();			  //close the input file
+	system("pause");		  //pause to view results
 	return 0;
 }//end main
 
+//-------------------------------------------------------------------------------------------------
+//readOperation: This function will read the char at the first index in the cstring array and 
+//				 determine the appropriate mathematical operation to be perfromed on the given
+//				 numerical values, located in the same cstring following the operation marker.
+//				 After determining the operation, the function will pass control of the program
+//				 to the corresponding function to perform the operation.
+//
+//Precondition:  This function needs the cstring array and the object of ifstream type sent to it.
+//Postcondition: This program will not return anything.
+//-------------------------------------------------------------------------------------------------
 void readOperation(const char readLine[MAX_LENGTH], ifstream& input){
 	int doContinue = 0;
 	int index = 0;
@@ -50,17 +137,17 @@ void readOperation(const char readLine[MAX_LENGTH], ifstream& input){
 
 	while (doContinue != 1){
 		std::cout << index;
-		if (isalpha(next)){
-			if (next == 'S' || next == 'T' || next == 'C'){
+		if (isalpha(next)){			//if char of alpha type (some type of letter)
+			if (next == 'S' || next == 'T' || next == 'C'){		//for trig types
 				trigFunction(readLine, next, index);
 				doContinue = 1;
 			}
 			else{
-				modeToPrint(readLine, next);
+				modeToPrint(readLine, next);		//all remaining alpha types
 				doContinue = 1;
 			}
 		}
-		else if (isprint(next) || (next == '•')){
+		else if (isprint(next) || (next == '•')){	//if char of print type (punctuation)
 			arithmetic(readLine, next, index);
 			doContinue = 1;
 		}
@@ -69,10 +156,22 @@ void readOperation(const char readLine[MAX_LENGTH], ifstream& input){
 	}
 }
 
+//-------------------------------------------------------------------------------------------------
+//readNumber:	 This function will find all of the numbers remaining in the cstring array, store
+//				 them in a new cstring array and then convert those cstrings into doubles. It will
+//				 then store those doubles into an array of doubles that the calling function has
+//				 access to. This function uses the cleanToNext function to take care of the white
+//				 spaces separating the numbers.
+//
+//Precondition:	 This function will require the cstring array, the array of type double, and the 
+//				 current index of the cstring array.
+//Postcondition: This function will not return anything.
+//-------------------------------------------------------------------------------------------------
 void readNumber(const char readLine[MAX_LENGTH], double numArray[2], int& index){
-	double answer = 0;
+	double answer = 0;		//stores the solution of the cast that will be stored in the new array
 	int j = 0, i = 0;
 	string digits = "";
+
 	for (unsigned int k = 0; (j != 2) && (k < (strlen(readLine) - 1)); k++){
 		if (isdigit(readLine[k]) || (readLine[k] == '.')){
 			digits += readLine[k];
@@ -86,62 +185,93 @@ void readNumber(const char readLine[MAX_LENGTH], double numArray[2], int& index)
 	}//end for
 }//end readNumber Function
 
+//-------------------------------------------------------------------------------------------------
+//arithmetic:    This function will perform the basic arithmetic operations on the two values sent
+//				 to it and then ouput the result. The function will use a switch statement to
+//				 determine what operation will be performed based on the char in the first index
+//				 of the cstring array.
+//
+//Precondition:  The function will need the cstring, the char at the first index of the cstring
+//				 array, and the index of the cstring.
+//Postcondition: The function will not return anything, but it will output the results of the 
+//				 operation.
+//-------------------------------------------------------------------------------------------------
 void arithmetic(const char readLine[MAX_LENGTH], char operation, int& index) {
-	double numArray[2];
+	double numArray[2];		//array that will hold the two numerical values still in the cstring
 	readNumber(readLine, numArray, index);
-	double numberOne = numArray[0];
-	double numberTwo = numArray[1];
+	double numberOne = numArray[0];	//take the first number out of the first index
+	double numberTwo = numArray[1]; //take the second number out of the second index
 	std::cout << numArray[0] << " " << numArray[1] << endl;
-	double answer;
-	switch (operation) {
-	case '+':
+	double answer;					//holds the solution to the operation
+	switch (operation) {			//switch to figure out the operation that will be used
+	case '+':						//case '+'
 		answer = numberOne + numberTwo;
 		std::cout << "+ " << numberOne << " " << numberTwo;
 		break;
-	case '-':
+	case '-':						//case '-'
 		answer = numberOne - numberTwo;
 		std::cout << "- " << numberOne << " " << numberTwo;
 		break;
-	case '•':
-	case '*':
+	case '•':						
+	case '*':						//case '*'
 		answer = numberOne * numberTwo;
 		std::cout << "* " << numberOne << " " << numberTwo;
 		break;
-	case '/':
-		answer = numberOne / numberTwo;
+	case '/':						//case '/'
+		if (numberOne > numberTwo)  //check to make sure the universe didnt implode
+			answer = numberOne / numberTwo;
+		else
+			answer = 0;				//just saved the universe
 		std::cout << "/ " << numberOne << " " << numberTwo;
 		break;
-	case '%':
+	case '%':						//case '%'
 		answer = static_cast<int>(numberOne) % static_cast<int>(numberTwo);
 		std::cout << "% " << numberOne << " " << numberTwo;
 		break;
-	case '^':
+	case '^':						//case '^'
 		std::cout << "^ " << numberOne << " " << numberTwo;
 		answer = pow(numberOne, numberTwo);
 		break;
-	default:
+	default:						//default, just ouput 0
 		answer = 0;
 		break;
 	}//end switch
 	std::cout << " " << answer << endl;
 }//end arthmetic Function
 
+//-------------------------------------------------------------------------------------------------
+//trigFunction:  This function will view the char holding the value that stands for the operation
+//				 needed to be performed. Using a switch statement, the function will match this
+//				 char with 'S', 'C', and 'T' and after finding the matching char, that trig
+//				 function will be selected. The function will then call readNumber to determine
+//				 the degree value that is within the cstring array. After recieving the value,
+//				 double is then converted from degree to radians using the following formula:
+//										PI(number)/180.
+//				 The function will then do the trig operation on the radian and finally output the
+//				 solution. 
+//
+//Precondition:  The function requires the cstring, first char in the cstring and the current
+//				 index of the cstring array to be sent to it.
+//Postcondition: The function will not return anything, but will change the value of the index
+//				 for the calling function.
+//-------------------------------------------------------------------------------------------------
 void trigFunction(const char readLine[MAX_LENGTH], char operation, int& index) {
-	double numArray[1];
-	readNumber(readLine, numArray, index);
-	double number = numArray[0];
-	double answer;
+	double numArray[1];						//array to hold the converted double
+	readNumber(readLine, numArray, index);  //find the number still in the cstring
+	double number = numArray[0];			//retrieve that double
+	number = (PI * number) / 180;			//convert from degrees to radians
+	double answer;							//holds the solution to the trig operation
 	switch (operation) {
-	case 'S':
-		answer = sin(PI*number / 180);
+	case 'S':								//case Sine
+		answer = sin(number);
 		std::cout << "S" << number;
 		break;
-	case 'C':
-		answer = cos(PI*number / 180);
+	case 'C':								//case Cosine
+		answer = cos(number);
 		std::cout << "C" << number;
 		break;
-	case 'T':
-		answer = tan(PI*number / 180);
+	case 'T':								//case Tangent
+		answer = tan(number);
 		std::cout << "T" << number;
 		break;
 	default:
@@ -151,6 +281,18 @@ void trigFunction(const char readLine[MAX_LENGTH], char operation, int& index) {
 	std::cout << " " << answer << endl;
 }//end trigFunction Function
 
+//-------------------------------------------------------------------------------------------------
+//modeToPrint:   This function will change the way decimal values are represented. A switch 
+//				 statement will check the char sent from readOperation and determine which decimal
+//			     structure should be used to print doubles. If an 'e' or 'E' is found, decimals
+//				 will be displayed in scientific notation. If a 'f' or 'F' is found, decimals will
+//				 be displayed with a fixed number of digits.
+//
+//Precondition:  The function requires a cstring and the char read from the first index of the 
+//				 cstring.
+//Postcondition: The function will not return anything. It will however, change the way all other
+//				 functions will print double variables.
+//-------------------------------------------------------------------------------------------------
 void modeToPrint(const char readLine[MAX_LENGTH], char next) {
 	switch (next) {
 	case 'e':
